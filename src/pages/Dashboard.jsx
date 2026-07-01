f3c35a909842f093dce4c142b9915fdba484b9df
KennedyyFelipee <Kennedyfelipe37@gmail.com>
2026-07-01 02:18:23 -0300

    creating domain

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -40,7 +40,7 @@ export default function Dashboard() {
     <div>
       <div className="dashboard-graph-container">
         <iframe 
-          src="http://localhost:8050/" 
+          src="http://44.217.81.127/dashboard/"
           style={{
             width: '100%',
             height: '700px',
